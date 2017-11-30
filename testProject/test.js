2c8bdbdddf22aa33d5ca44410dcd3f69962664a0
Server <tilesmaster@Tiless-MacBook-Pro.local>
2017-11-30 11:45:46 -0500

    added titles with spaces

--- testProject/test.js
+++ testProject/test.js
@@ -1,8 +1,23 @@
+
+
+
 // Include the request npm package (Don't forget to run "npm install request" in this folder first!)
 var request = require("request");
-var movieTitle = process.argv[2];
+var nodeArgs = process.argv;
+var movieTitle = "";
+
+for (var i = 2; i < nodeArgs.length; i++) {
+	if (i > 2 && i < nodeArgs.length){
+		movieTitle = movieTitle + "+" + nodeArgs[i];
+		console.log(movieTitle);
+	}
+	else {
+		movieTitle += nodeArgs[i];
+	}
+}
+var queryURL = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=40e9cece";
 // Then run a request to the OMDB API with the movie specified
-request("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+request(queryURL, function(error, response, body) {
 
   // If the request is successful (i.e. if the response status code is 200)
   if (!error && response.statusCode === 200) {
@@ -19,3 +34,47 @@ request("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=40e9c
   }
   
 });
+// //============
+// var request = require("request");
+
+// // Store all of the arguments in an array
+// var nodeArgs = process.argv;
+// console.log(nodeArgs);
+// // Create an empty variable for holding the movie name
+// var movieName = "";
+
+// // Loop through all the words in the node argument
+// // And do a little for-loop magic to handle the inclusion of "+"s
+// for (var i = 2; i < nodeArgs.length; i++) {
+//   if (i > 2 && i < nodeArgs.length) {
+//     movieName = movieName + "+" + nodeArgs[i];
+//   }
+//   else {
+//     movieName += nodeArgs[i];
+//   }
+// }
+
+// // Then run a request to the OMDB API with the movie specified
+// var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+// // This line is just to help us debug against the actual URL.
+// // console.log(queryUrl);
+
+// request(queryUrl, function(error, response, body) {
+
+//   // If the request is successful
+//   if (!error && response.statusCode === 200) {
+
+//     // Parse the body of the site and recover just the imdbRating
+//     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+//     var database = JSON.parse(body);
+//     	console.log(database);
+//         console.log(database.Year);
+//         console.log(database.Rated);
+//         console.log(database.BoxOffice);
+//         console.log(database.imdbRating);
+//         console.log(database.Actors);
+//   }
+// });
+
+
